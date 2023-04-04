a3ab50db605bf3a92ab078d5fe5b375e64d40adc
The-Flying-Dev <leandranm@outlook.com>
2023-04-04 21:56:44 +0200

    section 1

---
+++ Ruby Array - Initialization.rb
@@ -0,0 +1,20 @@
+#Problem 
+
+
+#Your task is to initialize three different variables as explained below.
+
+#Initialize an empty array with the variable name array
+
+#Initialize an array with exactly one nil element in it with the variable name array_1
+
+#Initialize an array with exactly two elements with value 10 in it using the variable name array_2. 
+
+
+#Solution
+
+array = []
+
+array_1 = [nil]
+
+array_2 = Array.new(2, 10)
+
---
+++ Ruby Control Structures - Until.rb
@@ -0,0 +1,21 @@
+#Problem 
+
+
+#"A hacker practices on HackerRank until getting to a rating of O(1) read as (Oh-one)"
+
+#Call the method coder.practice until coder.oh_one? becomes true.
+
+#Use the until control structure.
+
+#until is the logical equivalent of while not. 
+
+
+#Solution
+
+#1
+coder.practice until coder.oh_one?
+
+#2
+until coder.oh_one?
+  coder.practice
+end
